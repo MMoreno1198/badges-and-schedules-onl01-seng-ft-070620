3a1a380dce976aa310dae5bcde447628a1b0c066
Marcus Moreno <legendary.1198@gmail.com>
2020-07-09 22:46:32 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -19,7 +19,7 @@ def assign_rooms(attendees)
   
   attendees.each_with_index.map do |attendee, index|
     
-    "Hello, #{attendee}! You'll be assigned to room #{index+1}!"
+    "Hello, #{attendee}! You'll be assigned to room #{index+3}!"
     
   end
 end
